80ab4761d9ca91e91ccd798366ad9d593464f868
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2024-12-19 21:09:35 +0800

    Create SavingsAccount.java

---
+++ SavingsAccount.java
@@ -0,0 +1,46 @@
+package decoratorPattern;
+
+public class SavingsAccount implements BankAccount{
+    private String accountNumber;
+    private String accountName;
+    private double balance;
+
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+    public void setAccountNumber(String accountNumber){
+        this.accountNumber = accountNumber;
+    }
+    public String getAccountName(){
+        return accountName;
+    }
+    public void setAccountName(String accountName){
+        this.accountName = accountName;
+    }
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+
+    public String showAccountType(){
+        return "Savings Account";
+    }
+    public double getInterestRate(){
+        return 0.01;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public String showBenefits(){
+        return "Standard Savings Account";
+    }
+    public double computeBalanceWithInterest(){
+        return balance+balance*getInterestRate();
+    }
+    public String showInfo(){
+        String info = "Account number: "+accountNumber;
+        info += "\nAccount name: "+accountName;
+        info += "\nBalance: "+balance;
+        return info;
+    }
+
+}
